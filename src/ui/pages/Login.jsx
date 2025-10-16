6a31c1e375cf4246a6332b2bd2bba4ae4c05208b
50milvoltz <paulo.h.t.fox@gmail.com>
2025-10-15 21:02:53 -0300

    Estilização tela de login (não finalizada)

--- src/ui/pages/Login.jsx
+++ src/ui/pages/Login.jsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
-import "../App.css";
+import { useState } from "react"
+import "./Login.css"
 
 function Login() {
-  const [login, setLogin] = useState("");
-  const [senha, setSenha] = useState("");
-  const [mensagem, setMensagem] = useState("");
+  const [login, setLogin] = useState("")
+  const [senha, setSenha] = useState("")
+  const [mensagem, setMensagem] = useState("")
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
     try {
       const response = await fetch("http://127.0.0.1:3000/login", {
@@ -19,49 +19,59 @@ function Login() {
           login: login,
           senha_hash: senha,
         }),
-      });
+      })
 
-      const data = await response.json();
+      const data = await response.json()
 
       if (response.ok) {
-        window.electronAPI?.loginSuccess(data);
+        window.electronAPI?.loginSuccess(data)
       } else {
-        alert("Erro ao realizar login: ", data.message);
+        alert("Erro ao realizar login: ", data.message)
       }
     } catch (error) {
-      alert("Erro de conexão com servidor: ", error);
+      alert("Erro de conexão com servidor: ", error)
     }
-  };
+  }
 
   return (
     <div className="login-container">
       <form onSubmit={handleSubmit} className="login-box">
-        <h2>Login</h2>
-
-        <div className="input-group">
-          <label htmlFor="login">Usuário</label>
-          <input
-            type="text"
-            id="login"
-            placeholder="Digite seu usuário"
-            value={login}
-            onChange={(e) => setLogin(e.target.value)}
-            required
-          />
+        <div className="logo-name-box">
+          <h1 className="logo-name">
+            {" "}
+            <img
+              src="src\ui\assets\logo_hitsys.png"
+              alt="Logo"
+              className="logo"
+            />
+            HITSYS
+          </h1>
         </div>
+        <div className="inputs">
+          <div className="input-group">
+            <label htmlFor="login">Login</label>
+            <input
+              type="text"
+              id="login"
+              placeholder="Digite seu usuário"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+              required
+            />
+          </div>
 
-        <div className="input-group">
-          <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            id="senha"
-            placeholder="Digite sua senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-          />
+          <div className="input-group">
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              placeholder="Digite sua senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+          </div>
         </div>
-
         <button type="submit" className="btn-login">
           Entrar
         </button>
@@ -69,7 +79,7 @@ function Login() {
         {mensagem && <p className="mensagem">{mensagem}</p>}
       </form>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
