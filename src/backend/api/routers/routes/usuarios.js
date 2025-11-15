3a3a6bd8491df67206292ebf88652b5fe8a2df98
Murilo Avarino <muriloavarino@gmail.com>
2025-11-15 18:23:04 -0300

    Ajuste de Comentário
    ajustado comentário da nova rota de criação de usuário sem authenticação a fins de teste

--- src/backend/api/routers/routes/usuarios.js
+++ src/backend/api/routers/routes/usuarios.js
@@ -4,13 +4,14 @@ const user = require("../../controllers/UsuarioController");
 const auth = require("../../middlewares/UsuarioAuth");
 
 router.post("/cadastrar", auth.insere_usuario, user.sign_in);
-// Rota de teste (SEM autenticação) para facilitar criação de usuário via Postman
-// Uso: POST /usuarios/cadastrar-test com body { nome, login, senha_hash, id_perfil_usuario, ativo }
-// OBS: criada APENAS para testes; não usar em produção
-router.post("/cadastrar-test", user.sign_in);
 router.get("/visualizar", auth.visualiza_usuario, user.viewUsers);
 router.get("/visualizar/:id", user.viewUser);
 router.put("/atualizar/:id", auth.altera_usuario, user.updateUser);
 router.delete("/excluir/:id", auth.deleta_usuario, user.deleteUser);
+
+// Rota de teste (SEM autenticação) para facilitar criação de usuário via Postman
+// Uso: POST /usuarios/cadastrar-test com body { nome, login, senha_hash, id_perfil_usuario, ativo }
+// OBS: criada APENAS para testes; não usar em produção
+router.post("/cadastrar-test", user.sign_in);
 
 module.exports = router;
